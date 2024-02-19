8c47c708f22fd412459c51738752484e7e47337f
ShashidharShastri <124485154+ShashidharShastri@users.noreply.github.com>
2024-02-19 10:54:06 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
 mvn clean test -Dvideo.enabled=false & allure generate allure-results -c -o allure-report && allure open allure-report
+
+mvn clean test -Dvideo.enabled=false & allure generate allure-results -c -o allure-report && allure open allure-report
+
+mvn clean test -Dvideo.enabled=false & allure generate allure-results -c -o allure-report && allure open allure-report
